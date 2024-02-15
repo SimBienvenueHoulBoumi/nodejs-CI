6bd92f1a9d9eaf5581c4e0f1110f9e8d720ce4bd
Sim <brhulla@gmail.com>
2024-02-15 15:44:38 +0100

    remove views

--- index.ts
+++ index.ts
@@ -1,23 +1,12 @@
 import express from "express";
-import path from "path"; // Utilisation de path module pour gérer les chemins de fichiers
-
 
 const app = express();
 
 const port = 4000;
 
-// Définir le moteur de templates EJS
-app.set("view engine", "ejs");
-
-// Définir le chemin des vues
-app.set("views", path.join(__dirname, "views"));
-
 // Route pour afficher la vue home
 app.get("/", (req, res) => {
-  res.render("home", {
-    title: "Accueil",
-    message: "Bienvenue sur notre site !",
-  });
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
